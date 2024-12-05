9b203e9e49f8640060a9652895a94a5da438f468
Ubuntu <ubuntu@ip-172-31-34-33.us-east-2.compute.internal>
2024-12-05 04:46:37 +0000

    cleaning up comments

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -51,16 +51,6 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     out_pool_height = out_height // pool_size
     out_pool_width = out_width // pool_size
 
-    # wff_i_0 = nl.arange(filter_height)[:, None, None, None]
-    # wff_i_1 = nl.arange(filter_width)[None, :, None, None]
-    # wff_i_2 = nl.arange(in_channels)[None, None, :, None]
-    # W_ff = nl.ndarray((filter_height, filter_width, in_channels, out_channels), dtype=W.dtype, buffer=nl.sbuf)
-    # for o in nl.affine_range(out_channels):
-    #     W_ff[wff_i_0, wff_i_1, wff_i_2, o] = nl.load(W[o, wff_i_2, wff_i_0, wff_i_1])
-    # print("W shape: ", W.shape)
-    # print("W_ff shape: ", W_ff.shape)
-    # print(f"out_height: {out_height}, out_width: {out_width}, out_pool_height: {out_pool_height}, out_pool_width: {out_pool_width}")
-    
     # Can assume multiple of 128 to avoid using mask
     assert in_channels % 128 == 0
 
@@ -74,109 +64,106 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
         buffer=nl.hbm,
     )
 
-    MAX_FREE_DIM = 65536
+    MAX_FREE_DIM = 65536 # maximum FREE_DIM on sbuf tiles (physical constraint)
+
     # Various tiling dimensions (You may want to define more of them)
     TILE_C_OUT = min(out_channels, nl.tile_size.gemm_stationary_fmax) 
     n_tiles_c_out = out_channels // TILE_C_OUT
-
     TILE_C_IN = min(in_channels, nl.tile_size.pmax)
     n_tiles_c_in = in_channels // TILE_C_IN
-    # print(f"TILE_C_OUT: {TILE_C_OUT}, n_tiles_c_out: {n_tiles_c_out}, TILE_C_IN: {TILE_C_IN}, n_tiles_c_in: {n_tiles_c_in}")
 
-    # TILE_HW is the largest multiple of pool_size * input_width that is less than or equal to nl.tile_size.gemm_moving_fmax
-    # I assume that pool_size * input_width is always less than or equal to nl.tile_size.gemm_moving_fmax
-    # print(f"pool_size: {pool_size}, input_width: {input_width}, gemm_moving: {nl.tile_size.gemm_moving_fmax}")
-    TILE_HW = (pool_size * out_width) * (nl.tile_size.gemm_moving_fmax // (pool_size * out_width))
+    # TILE_HW is the number of output pixels per psum tile, given nl.tile_size.gemm_moving_fmax
+    TILE_HW = (pool_size * out_width) * (nl.tile_size.gemm_moving_fmax // (pool_size * out_width)) 
     TILE_HW = min(TILE_HW, out_height * out_width)
-    n_vert_pools = TILE_HW // (pool_size * out_width)
-    TILE_H = TILE_HW // out_width
-    # print(f"TILE_HW: {TILE_HW}, n_tiles_hw: {n_tiles_hw}, n_vert_pools: {n_vert_pools}, TILE_H: {TILE_H}")
+    n_vert_pools = TILE_HW // (pool_size * out_width) # number of rows of output per psum tile
+    TILE_H = TILE_HW // out_width # number of rows of input per psum tile
 
-    # Tiling dimensions for loading input into sbuf
-    n_conv_rows = MAX_FREE_DIM // (input_width * n_tiles_c_in) # number of conv rows per s_tile
+    # S_TILE_HW is the largest number of input elements that will fit into one s_tile in sbuf, given MAX_FREE_DIM
+    n_conv_rows = MAX_FREE_DIM // (input_width * n_tiles_c_in) # number of input rows in one s_tile
     n_conv_rows = min(n_conv_rows, input_height)
     n_tiles_hw = (n_conv_rows - (filter_height - 1)) // TILE_H # number of input psum tiles per s_tile
-    while out_height % (TILE_H * n_tiles_hw) != 0: # inelegant
+    while out_height % (TILE_H * n_tiles_hw) != 0: # ensure s_tiles perfectly tile the image
         n_tiles_hw -= 1
     n_conv_rows = n_tiles_hw * TILE_H
     S_TILE_H = n_conv_rows + (filter_height - 1) # number of rows of input per s_tile
-    S_TILE_HW = S_TILE_H * input_width * n_tiles_c_in # real size of FREE_DIM
-    n_tiles_s_in = out_height // (TILE_H * n_tiles_hw)
-    # print(f"n_tiles_s_in: {n_tiles_s_in}, n_vert_pools: {n_vert_pools}, n_tiles_hw: {n_tiles_hw} in_channels: {in_channels}, n_tiles_c_in: {n_tiles_c_in}, S_TILE_H: {S_TILE_H}, S_TILE_HW: {S_TILE_HW}, n_conv_rows: {n_conv_rows}, n_tiles_hw: {n_tiles_hw}, TILE_H: {TILE_H}, filter_height: {filter_height}")
+    S_TILE_HW = S_TILE_H * input_width * n_tiles_c_in
+    n_tiles_s_in = out_height // (TILE_H * n_tiles_hw) # number of s_tiles per image
 
-    # Process the images in batches
+    # process the images in batches
     for b in nl.affine_range(batch_size):
-        # TODO: Perform the convolution of X[b] with the weights W and bias b, followed by a maxpool
-        # and store the result in X_out[b]
 
-        # first load bias and weights into SBUF to reduce redundant loads later on
+        # bias fits entirely into sbuf
         bias_tiles = nl.ndarray((TILE_C_OUT, n_tiles_c_out), dtype=bias.dtype, buffer=nl.sbuf)
         for n in nl.affine_range(n_tiles_c_out):
             bias_tiles[:, n] = nl.load(bias[n * TILE_C_OUT:(n + 1) * TILE_C_OUT])
-        # Assume that (in_channels * filter_height * filter_width) < MAX_FREE_DIM
+        # W fits entirely into sbuf
         weight_tiles = nl.ndarray((n_tiles_c_in, nl.par_dim(TILE_C_IN), out_channels, filter_height, filter_width), dtype=bias.dtype, buffer=nl.sbuf)
         for k in nl.affine_range(n_tiles_c_in):
             for l in nl.affine_range(out_channels):
                 weight_tiles[k, :, l, :, :] = nl.load(W[l, k * TILE_C_IN:(k + 1) * TILE_C_IN, :, :])
         
-        # convolution first 
+        # process the image over s_tiles
         for s in nl.affine_range(n_tiles_s_in):
-            # load in s_input
-            s_input = nl.ndarray((n_tiles_c_in, nl.par_dim(TILE_C_IN), S_TILE_H, input_width), dtype=X.dtype, buffer=nl.sbuf)
+            # load in input
+            s_tile = nl.ndarray((n_tiles_c_in, nl.par_dim(TILE_C_IN), S_TILE_H, input_width), dtype=X.dtype, buffer=nl.sbuf)
             for k in nl.affine_range(n_tiles_c_in):
-                s_input[k, :, :, :] = nl.load(X[b, k * TILE_C_IN:(k + 1) * TILE_C_IN, s * n_conv_rows:(s * n_conv_rows + S_TILE_H), :])
+                s_tile[k, :, :, :] = nl.load(X[b, k * TILE_C_IN:(k + 1) * TILE_C_IN, s * n_conv_rows:(s * n_conv_rows + S_TILE_H), :])
+            
+            # process the s_tile over batches of 128 output channels
             for n in nl.affine_range(n_tiles_c_out):
-                # copy in bias tile
+                # copy in bias tile from SBUF
                 bias_ = nl.ndarray((TILE_C_OUT,1,1), dtype=bias.dtype, buffer=nl.sbuf)
                 bias_[...] = nl.copy(bias_tiles[:, n])
                 broadcasted_bias = bias_.broadcast_to((TILE_C_OUT, n_vert_pools, out_pool_width))
-                # copy in weight tile
+                # copy in weight tile from SBUF
                 weight_ = nl.ndarray((n_tiles_c_in, nl.par_dim(TILE_C_IN), TILE_C_OUT, filter_height, filter_width), dtype=W.dtype, buffer=nl.sbuf)
                 for k in nl.affine_range(n_tiles_c_in):
                     weight_[k, :, :, :, :] = nl.copy(weight_tiles[k, :, n * TILE_C_OUT:(n + 1) * TILE_C_IN, :, :])
+                
+                # process the s_tile over strips of input (< 512 pixels)that will fit into psum
                 for m in nl.affine_range(n_tiles_hw):
                     conv_result = nl.zeros((TILE_C_OUT, TILE_H * out_width), dtype=X_out.dtype, buffer=nl.sbuf)
-                    # copy in input tile
+                    # copy in input tile strip from s_tile
                     input_ = nl.ndarray((n_tiles_c_in, nl.par_dim(TILE_C_IN), TILE_H + filter_height - 1, out_width + filter_width - 1), dtype=X.dtype, buffer=nl.sbuf)
                     for k in nl.affine_range(n_tiles_c_in):
                         for h in nl.affine_range(TILE_H + filter_height - 1):
                             true_h = m * TILE_H + h
-                            input_[k, :, h, :] = nl.copy(s_input[k, :, true_h, :])
+                            input_[k, :, h, :] = nl.copy(s_tile[k, :, true_h, :])
+                    
+                    # compute convolution over this psum tile
                     for i in nl.affine_range(filter_height):
                         for j in nl.affine_range(filter_width):
-                            # Matrix multiplication    
                             res_psum = nl.zeros((TILE_C_OUT, TILE_H * out_width), nl.float32, buffer=nl.psum)
+                            # process the tile over batches of 128 input channels
                             for k in nl.affine_range(n_tiles_c_in):
+                                # shift the input tile by (i, j)
                                 rhs_tile = nl.ndarray((TILE_C_IN, TILE_H * out_width), dtype=X.dtype, buffer=nl.sbuf)
-                                # rhs_tile = nl.copy(input_[k, :, i:(i + TILE_H), j:j+out_width]).reshape((TILE_C_IN, TILE_H * out_width))
                                 for h in nl.affine_range(TILE_H):
                                     rhs_tile[:, h*out_width:(h+1)*out_width] = nl.copy(input_[k, :, h + i, j:j+out_width], dtype=X.dtype)
-                                # Accumulate partial-sums into PSUM
+                                # multiply by the weight at filter position (i, j)
                                 res_psum += nl.matmul(weight_[k, :, :, i, j], rhs_tile, transpose_x=True)
-                            # Copy the result from PSUM back to SBUF, and cast to expected output data-type
                             conv_result += nl.copy(res_psum, dtype=X_out.dtype)
-
+                    
+                    # reshape convolution output to X_out dimensions
                     conv_result_reshaped = nl.ndarray((TILE_C_OUT, TILE_H, out_width), dtype=X.dtype, buffer=nl.sbuf)
                     for h in nl.affine_range(TILE_H):
                         conv_result_reshaped[:, h, :] = conv_result[:, h * out_width:(h + 1) * out_width]
 
                     if pool_size > 1:
+                        # with maxpool
                         i_0 = nl.arange(TILE_C_OUT)[:, None, None, None, None]
                         i_1 = nl.arange(n_vert_pools)[None, :, None, None, None]
                         i_2 = nl.arange(pool_size)[None, None, :, None, None]
                         i_3 = nl.arange(out_pool_width)[None, None, None, :, None]
                         i_4 = nl.arange(pool_size)[None, None, None, None, :]
-                        # out_tile = nl.max(conv_result_reshaped[i_0, pool_size * i_1 + i_2, pool_size * i_3 + i_4], axis=[2, 4])
                         out_tile = nl.max(conv_result_reshaped[i_0, pool_size * i_1 + i_2, pool_size * i_3 + i_4], axis=[2, 4])
                         out_tile = out_tile + broadcasted_bias
                         tile_idx = n_tiles_hw * s + m
                         nl.store(X_out[b, n * TILE_C_OUT:(n + 1) * TILE_C_OUT, tile_idx * n_vert_pools:(tile_idx + 1) * n_vert_pools, :], value=out_tile)
                     else:
-                        # print("no maxpooling, assume out_pool_width = out_width")
+                        # without maxpool
                         out_tile = conv_result_reshaped + broadcasted_bias
                         tile_idx = n_tiles_hw * s + m
                         nl.store(X_out[b, n * TILE_C_OUT:(n + 1) * TILE_C_OUT, tile_idx * TILE_H:(tile_idx + 1) * TILE_H, :], value=out_tile)
-
-
 
     return X_out
